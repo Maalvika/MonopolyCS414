c2a788d13aa65327896c1d31a8bcbae2086804d3
ayusmittal <ayusmittal@gmail.com>
2016-10-18 23:49:42 -0600

    both vertical grid implemented

--- Cards.java
+++ Cards.java
@@ -13,6 +13,10 @@ public class Cards {
 	int rent2RROwned;
 	int rent3RROwned;
 	int rent4RROwned;
+	int rentFirstUtility;
+	int rentSecondUtility;
+	int incomeTax;
+	int luxuryTax;
 	
 	int verticalFirstGrid[][]= {{0,0,0,0,0,0,0,0,0,0},{60,2,10,30,90,160,250,30,50,50},{0,0,0,0,0,0,0,0,0,0},
 			              {60,2,10,30,90,160,250,30,50,50},{200,0,0,0,0,0,0,0,0,0},
@@ -29,7 +33,7 @@ public class Cards {
 	int verticalSecondGrid[][]= {{0,0,0,0,0,0,0,0,0,0},{220,18,90,250,700,875,1050,110,150,150},{0,0,0,0,0,0,0,0,0,0},
 			{220,18,90,250,700,875,1050,110,150,150},{240,20,100,300,750,925,1100,120,150,150},
             {200,25,0,50,100,200,100,0,0,0,0},{260,22,110,330,800,975,1150,130,150,150},
-            {260,22,110,330,800,975,1150,130,150,150},{150,20,100,0,0,0,0,0,0,0},
+            {260,22,110,330,800,975,1150,130,150,150},{150,20,100,75,0,0,0,0,0,0},
             {280,24,120,360,850,1025,1200,140,150,150},{0,0,0,0,0,0,0,0,0,0}};
 	
 	int horizontalBelowGrid[][]= {{0,0,0,0,0,0,0,0,0,0},{300,26,130,390,900,1100,1275,150,200,200},{300,26,130,390,900,1100,1275,150,200,200},
@@ -41,6 +45,10 @@ public class Cards {
 	
 	
 	
+	public Cards()
+	{
+		//Constructor
+	}
 	public int[] setCards(int i,int j){
 		if (j==0){
 			//vertical first grid
@@ -53,6 +61,16 @@ public class Cards {
 						{
 							value[k] = verticalFirstGrid[temp][k];
 						}
+					this.cost = value[0];
+					this.rentInitial = value[1];
+					this.rentFirstHouse = value[2];
+					this.rentSecondHouse = value[3];
+					this.rentThirdHouse = value[4];
+					this.rentFourthHouse= value[5];
+					this.rentHotel = value[6];
+					this.mortgageValue = value[7];
+					this.houseCost = value[8];
+					this.hotelCost = value[9];
 					return value;
 			}
 			if(i==5)
@@ -63,19 +81,78 @@ public class Cards {
 				{
 					value[k]= verticalFirstGrid[10-i][k];	
 				}
+				this.cost = value[0];
+				this.rentInitial = value[1];
+				this.rent2RROwned = value[3];
+				this.rent3RROwned = value[4];
+				this.rent4RROwned = value[5];
+				this.mortgageValue = value[6];	
+				return value;
+			}
+			if (i==4)
+			{
+				incomeTax = 200;
+				int[] value ={incomeTax};
 				return value;
 			}
 		}
-		if (j==10){
+		if (j==10)
+		{
 			//vertical second grid
+			if ((i==1)|(i==3)|(i==4)|(i==6)|(i==7)|(i==9))
+			{
+				int value[]={cost, rentInitial, rentFirstHouse, rentSecondHouse, rentThirdHouse, rentFourthHouse,
+					     rentHotel, mortgageValue, houseCost,hotelCost};
+					for (int k=0; k<10; k++)
+						{
+							value[k] = verticalFirstGrid[10-i][k];
+						}
+					this.cost = value[0];
+					this.rentInitial = value[1];
+					this.rentFirstHouse = value[2];
+					this.rentSecondHouse = value[3];
+					this.rentThirdHouse = value[4];
+					this.rentFourthHouse= value[5];
+					this.rentHotel = value[6];
+					this.mortgageValue = value[7];
+					this.houseCost = value[8];
+					this.hotelCost = value[9];
+					return value;
+			}
+			if(i==5)
+			{
+				int temp = 10-1;
+				int value[] = {cost,rentInitial,0,rent2RROwned,rent3RROwned, rent4RROwned, mortgageValue};
+				for (int k =0 ;k<7;k++)
+				{
+					value[k]= verticalFirstGrid[10-i][k];	
+				}
+				this.cost = value[0];
+				this.rentInitial = value[1];
+				this.rent2RROwned = value[3];
+				this.rent3RROwned = value[4];
+				this.rent4RROwned = value[5];
+				this.mortgageValue = value[6];				
+				return value;
+			}
+			if (i==8)
+			{
+				this.cost =150;
+				this.rentFirstUtility = 20;
+				this.rentSecondUtility = 100;
+				this.mortgageValue = 75;
+			}
+			
 		}
-		if (i==0){
+		//if (i==0){
 			//horizontal above grid
-		}
-		if (i ==10){
+		//}
+		//if (i ==10){
 			//horizontal below grid
-		}
+		//}
 		//System.out.println(information[1][1]);
+		int[] value={0,0};
+		return value;
 	}
 	public void getCardsValue(int i, int j){
 		int[] information = setCards(i,j);
@@ -83,9 +160,6 @@ public class Cards {
 		//System.out.println(information[1][1]);
 	}
 
-	public static void main(String []args){
-		int information[][]= {{0,0,0,1},{1,1,1,1}};
-		System.out.println(information);
-	}
+	
 }
 
