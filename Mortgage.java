c2ed4828275ebcdec355ccd10ae035c9214104cf
ayusmittal <ayusmittal@gmail.com>
2016-10-19 18:34:21 -0600

    Mortgage edits

--- Mortgage.java
+++ Mortgage.java
@@ -1,8 +1,34 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Mortgage {
-
+	
+	
+	Set<Cards> mortgageSet = new HashSet<Cards>();
+	
 	public Mortgage() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public int mortgageProperty(int i, int j)
+	{
+		Cards cs = new Cards(i,j);
+		return cs.mortgageValue;
+		
+	}
+	
+	public void addPropertyMortgage(Cards c)
+	{
+		mortgageSet.add(c);
+	}
+	
+	public void unMortgageProperty()
+	{
+		
+	}
+	
+	public void removePropertyMortgage(Cards c)
+	{
+		mortgageSet.remove(c);
+	}
 }
