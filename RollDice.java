2db75f935a212436fd506d7fa029b376c0a28ba2
nimratasingh <nimrata@colostate.edu>
2016-10-22 00:44:12 -0600

    Update RollDice.java

--- RollDice.java
+++ RollDice.java
@@ -2,7 +2,7 @@ public class Dice {
 
 	public static int rollDice(){
 		int die1;  //The number that comes up on the 1st die
-	    int die2;  //The number that comes up on the 2nd die
+	        int die2;  //The number that comes up on the 2nd die
 		  //The sum of numbers that came up on both the dice
 		
 		die1 = (int)(Math.random()*6) + 1;
